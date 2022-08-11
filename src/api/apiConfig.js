f38ba078fbf96c73143dc59b69cf1230486f9fda
Miguel <mwicke@gmail.com>
2022-08-11 11:41:34 +0200

    test1

---
+++ src/api/apiConfig.js
@@ -0,0 +1,7 @@
+const apiConfig = {
+  baseUrl: "http://api.themoviedb.org/3/",
+  apiKey: "2da6983525f4293e8ec9e1cd1851d3d2",
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+};
+
+export default apiConfig;
