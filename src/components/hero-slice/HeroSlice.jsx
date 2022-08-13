5a4345792d9ce9012bfe61a750f2138012126651
Miguel <mwicke@gmail.com>
2022-08-13 14:22:43 +0200

    json update

--- src/components/hero-slice/HeroSlice.jsx
+++ src/components/hero-slice/HeroSlice.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 import tmdbApi, { category, movieType } from "../../api/tmdbApi";
 import apiConfig from "../../api/apiConfig";
 
 const HeroSlice = () => {
+
+    const [movieItems, setMovieItems] = useState([]);
+
+    useEffect(() => {
+        const getMovies = async () => {
+            const params = {page: 1};
+            try {
+                const response = await tmdbApi.get(`/discover/movie`, {params});
+                setMovieItems(response.data.results);
+            }
+        }
+    }, []);
+
   return <div className="hero-slide">HeroSlice</div>;
 };
 
