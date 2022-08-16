7fe9082f7ce7bccb611f4a995c0f20ba0eaed2e0
Miguel <mwicke@gmail.com>
2022-08-16 11:49:35 +0200

    movie-grid

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./input.scss";
 
-const Input = () => {
+const Input = (props) => {
   return (
     <input
       type={props.type}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { OutlineButton } from "../components/button/Button";
 
 import HeroSlice from "../components/hero-slice/HeroSlice";
 
@@ -6,6 +8,16 @@ const Home = () => {
   return (
     <>
       <HeroSlice />
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending Movies</h2>
+            <Link to="/movie">
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
