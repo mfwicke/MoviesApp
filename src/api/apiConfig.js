19045e407d69b1ba79a2931d618fab0537462ccf
Miguel <mwicke@gmail.com>
2022-08-17 10:41:34 +0200

    vercel4

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-  baseUrl: "http://api.themoviedb.org/3/",
+  baseUrl: "https://api.themoviedb.org/3/",
   apiKey: "2da6983525f4293e8ec9e1cd1851d3d2",
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
